d8c0223c59c730ff86678559b1480652a76e3876
Abel63 <ozimandiya@gmail.com>
2016-04-17 13:09:46 +0500

    m6e3 Convertation strings to numbers

---
+++ src/FromString.java
@@ -0,0 +1,20 @@
+/**
+ * Created by kollaps on 16.04.16.
+ */
+public class FromString {
+
+    public int fromString(final char[] chars) {
+        if (chars.length == 0) return 0;
+        int result = 0;
+        for (int i = 0; i < 3 && i < chars.length; i++) {
+            if (i == 0 && chars[i] == '-') continue;
+            final char ch = chars[i];
+            final int r = Integer.valueOf(String.valueOf(ch));
+            result *= 10;
+            result += r;
+
+        }
+        return chars[0] == '-' ? -result : result;
+    }
+
+}
---
+++ src/ValueOfDemo.java
@@ -0,0 +1,32 @@
+import static java.lang.Float.parseFloat;
+
+/**
+ * Created by kollaps on 16.04.16.
+ */
+public class ValueOfDemo {
+    public static void main(String[] args) {
+
+        // this program requires two
+        // arguments on the command line
+        if (args.length == 2) {
+            // convert strings to numbers
+            float a = Float.parseFloat(args[0]);                // 2 различных способа
+            float b = (Float.valueOf(args[1])).floatValue();
+
+            // do some arithmetic
+            System.out.println("a + b = " +     // Выполняется конкатенация строк
+                    (a + b));
+            System.out.println("a - b = " +
+                    (a - b));
+            System.out.println("a * b = " +
+                    (a * b));
+            System.out.println("a / b = " +
+                    (a / b));
+            System.out.println("a % b = " +
+                    (a % b));
+        } else {
+            System.out.println("This program " +
+                    "requires two command-line arguments.");
+        }
+    }
+}
